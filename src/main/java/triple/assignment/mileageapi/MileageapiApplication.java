d0958a32617f8735f9979f37cfb5afc39a0594c0
junyeong <adipy1470@naver.com>
2022-07-01 00:01:52 +0900

    add new project

---
+++ src/main/java/triple/assignment/mileageapi/MileageapiApplication.java
@@ -0,0 +1,13 @@
+package triple.assignment.mileageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MileageapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MileageapiApplication.class, args);
+	}
+
+}
