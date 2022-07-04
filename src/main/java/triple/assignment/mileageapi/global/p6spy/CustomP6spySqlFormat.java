eac73d68cc010f36e768d8938f26798e2489da84
junyeong <adipy1470@naver.com>
2022-07-04 21:08:48 +0900

    feat: 쿼리 로깅 위한 p6spy 추가

---
+++ src/main/java/triple/assignment/mileageapi/global/p6spy/CustomP6spySqlFormat.java
@@ -0,0 +1,39 @@
+package triple.assignment.mileageapi.global.p6spy;
+
+import com.p6spy.engine.logging.Category;
+import com.p6spy.engine.spy.appender.MessageFormattingStrategy;
+import org.hibernate.engine.jdbc.internal.FormatStyle;
+
+import java.util.Locale;
+
+public class CustomP6spySqlFormat implements MessageFormattingStrategy {
+    @Override
+    public String formatMessage(int connectionId,
+                                String now,
+                                long elapsed,
+                                String category,
+                                String prepared,
+                                String sql,
+                                String url) {
+        sql = formatSql(category, sql);
+        return now + " | " + elapsed + "ms | " + category + " | connection " + connectionId + " | " + sql;
+    }
+
+    private String formatSql(String category, String sql) {
+        if (  sql == null || "".equals(sql.trim())  ) {
+            return sql;
+        }
+
+        // Only format Statement, distinguish DDL And DML
+        if (  Category.STATEMENT.getName().equals(category)  ) {
+            String tempSql = sql.trim().toLowerCase(Locale.ROOT);
+            if (  tempSql.startsWith("create") || tempSql.startsWith("alter") || tempSql.startsWith("comment")  ) {
+                sql = FormatStyle.DDL.getFormatter().format(sql);
+            }
+            else {
+                sql = FormatStyle.BASIC.getFormatter().format(sql);
+            }
+        }
+        return sql;
+    }
+}
