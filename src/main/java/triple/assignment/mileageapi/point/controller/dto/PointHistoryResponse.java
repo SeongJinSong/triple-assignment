20023abb3ae38b9c86dd534500c409134fd872ef
junyeong <adipy1470@naver.com>
2022-07-06 22:14:15 +0900

    fix: setter 추가

--- src/main/java/triple/assignment/mileageapi/point/controller/dto/PointHistoryResponse.java
+++ src/main/java/triple/assignment/mileageapi/point/controller/dto/PointHistoryResponse.java
@@ -3,12 +3,14 @@ package triple.assignment.mileageapi.point.controller.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import triple.assignment.mileageapi.point.domain.Point;
 
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+@Setter
 @Getter
 @Builder
 @AllArgsConstructor
